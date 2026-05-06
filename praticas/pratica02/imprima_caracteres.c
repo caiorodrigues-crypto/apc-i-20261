f4abf35df0149880a27c056fa7565b4b185a5163
caiorodrigues-crypto <caiogabriel9812@gmail.com>
2026-05-06 09:24:13 -0300

    codigo da pratica02

--- praticas/pratica02/imprima_caracteres.c
+++ praticas/pratica02/imprima_caracteres.c
@@ -2,11 +2,8 @@
 
 int main() {
 
-char ch ='A';
-char str = "Isso aqui eh uma string";
-
 printf("%c\n", 'A');
-printf("%s\n", 'Isso aqui eh uma string');
+printf("%s\n", "Isso aqui eh uma string");
 
     return 0;
 }
