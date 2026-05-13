115797f619d3f2cb7220da455b2f86f8432bbb2e
caiorodrigues-crypto <caiogabriel9812@gmail.com>
2026-05-13 08:30:13 -0300

    garatir que salvou

--- praticas/pratica03/declara_variaveis.c
+++ praticas/pratica03/declara_variaveis.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main() {
 
+// Declarar variáveis para armazenar a idade, o sexo (M ou F), a altura (ex.: 1.75f)
+// e o peso (ex.: 72.845) de uma pessoa e,
+// em seguida, imprimir todos esses valores utilizando
+// os especificadores de formato corretos;
 
 
 
@@ -10,6 +14,5 @@ int main() {
 
 
 
-    
     return 0;
 }
